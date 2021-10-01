28f5d5814250f6133f09c9a038fb610681a564c5
jedi <amajidsinar@gmail.com>
2021-10-01 18:33:05 +0700

    add ballot contract

---
+++ contracts/Ballot.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.8.0;
+
+contract Ballot{
+    address[] public players;
+    mapping(address=>uint256) public balances;
+    
+    function enterBallot() public payable{
+        require(msg.value > 0.01 ether);
+        balances[msg.sender] += msg.value;
+        players.push(msg.sender);
+    }
+    function random() private view returns(uint){
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+    }
+    
+    function pickWinner() public view returns(address){
+        uint random_number = random() % players.length;
+        return players[random_number];
+    }
+    function distributeReward(address payable winner) public {
+        uint rewardAmount = address(this).balance;
+        winner.transfer(rewardAmount);
+    }
+        
+    function resetBallot() public{
+        players = new address[](0);
+    }
+    
+    function getAtStake() public view returns(uint256){
+        return address(this).balance;
+    }
+    
+} 
+    
