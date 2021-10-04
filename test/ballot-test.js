5f7048d400b05e40aee01a04203d1f13369dcfe7
jedi <amajidsinar@gmail.com>
2021-10-05 01:31:24 +0700

    fix test

--- test/ballot-test.js
+++ test/ballot-test.js
@@ -4,33 +4,22 @@ const { ethers, waffle } = require("hardhat");
 
 
 describe("Ballot", function () {
-    it("Check ballot basic functionality", async function() {
-        beforeEach(async function() {
-            const Ballot = await ethers.getContractFactory("Ballot")
-            const ballot = await Ballot.deploy()
-            await ballot.deployed()
-            
-            const provider = waffle.provider
-            const accounts = await ethers.getSigners()
-        })
-        it("check manager status", async function(){
-            var balanceManager = await provider.getBalance(accounts[0].address)
-            var balanceManager = ethers.utils.formatEther(balanceManager)
-            console.log(typeof(balanceManager))
-            // expect(balanceManager) 
-        })
-        
+    it("Check ballot basic functionality", async() => {
+        const Ballot = await ethers.getContractFactory("Ballot")
+        const ballot = await Ballot.deploy()
+        await ballot.deployed()
+
+        const provider = waffle.provider
+        const accounts = await ethers.getSigners()
+
+        var balanceManager = await provider.getBalance(accounts[0].address)
+        var balanceManager = ethers.utils.formatEther(balanceManager)
 
         var balanceUser0 = await provider.getBalance(accounts[1].address)
-        var balanceUser0 = ethers.utils.formatEther(balanceUser0) 
-        
-           
-        
-        // var balance
-
-
-        console.log(balanceManager)
-
+        var balanceUser0 = ethers.utils.formatEther(balanceUser0)
+        expect(balanceUser0).to.equal('10000.0')
+        expect(balanceManager).to.not.equal('10000.0')
+        })
     });
     
-});
+
